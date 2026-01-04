c12a5fb4bc9ab4c921759f13856c95cced136438
nutant <65939836+nutant233@users.noreply.github.com>
2026-01-04 13:37:05 +0800

    fix

--- src/main/java/fast/fastrecipesearch/compat/Polymorph.java
+++ src/main/java/fast/fastrecipesearch/compat/Polymorph.java
@@ -14,9 +14,8 @@ import net.minecraft.world.World;
 public class Polymorph {
 
     public static <I extends RecipeInput, T extends Recipe<I>> RecipeEntry<T> getBlockEntityRecipe(RecipeManager manager, RecipeType<T> type, I inventory, World level) {
-        IRecipeData<?> data = (IRecipeData<?>) ((IRecipeContext) manager).polymorph$getContext();
-        if (data instanceof BlockEntity blockEntity) {
-            data = PolymorphApi.getInstance().getBlockEntityRecipeData(blockEntity);
+        if (((IRecipeContext) manager).polymorph$getContext() instanceof BlockEntity blockEntity) {
+            IRecipeData<?> data = PolymorphApi.getInstance().getBlockEntityRecipeData(blockEntity);
             if (data != null) {
                 return PolymorphApi.getInstance().getRecipeManager().getBlockEntityRecipe(type, inventory, level, blockEntity).orElse(null);
             }
